d1e76bbe68408dffc0a2a006e16fe8967357ad26
BrunoASNascimento <bruno-asn@outlook.com>
2025-02-08 14:43:48 -0300

    Add FilmController and Film class; remove WeatherForecast
    Removed WeatherForecastController.cs and WeatherForecast.cs files.
    Updated launchSettings.json to disable browser launch for films_api.
    Added FilmController.cs with AddFilm method for managing films.
    Added Film.cs with properties and validation for film details.

---
+++ films_api/Controllers/FilmController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using films_api.Models;
+
+namespace films_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmController : ControllerBase
+    {
+        private static List<Film> films = new List<Film>();
+
+        [HttpPost]
+        public void AddFilm([FromBody] Film film)
+        {
+
+            films.Add(film);
+            Console.WriteLine("Film added: " + film.Title);
+
+        }
+    }
+}
---
+++ films_api/Models/Film.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace films_api.Models
+{
+    public class Film
+    {
+        [Required(ErrorMessage ="The Title of the film is required.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "The Genre of the film is required.")]
+        [MaxLength(50, ErrorMessage = "The length of Genre cannot exceed 50 characters.")]
+        public string Genre { get; set; }
+        [Required]
+        [Range(60,600, ErrorMessage ="The duration needs to be between 60 and 600 minutes.")]
+        public int Duration { get; set; }
+    }
+}
