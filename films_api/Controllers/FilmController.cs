c2c5e026f6a50ffa7581509ae4a4c75925252252
BrunoASNascimento <bruno-asn@outlook.com>
2025-02-23 15:43:13 -0300

    Refactor FilmController and add AddFilm method
    Refactored FilmController to use class-level attributes and fields.
    Added constructor for dependency injection of FilmContext and IMapper.
    Introduced AddFilm method to add new films using CreateFilmDTO.
    Updated GetFilms and GetFilmById to use class-level _context.

--- films_api/Controllers/FilmController.cs
+++ films_api/Controllers/FilmController.cs
@@ -4,45 +4,44 @@ using films_api.Data;
 using films_api.Data.DTOs;
 using AutoMapper;
 
-namespace films_api.Controllers
+namespace films_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class FilmController : ControllerBase
+    private FilmContext _context;
+    private IMapper _mapper;
+
+
+    public FilmController(FilmContext context,IMapper mapper)
     {
-        private FilmContext _context;
-        private IMapper _mapper;
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public IActionResult AddFilm([FromBody] CreateFilmDTO filmDTO)
+    {
+        Film film = _mapper.Map<Film>(filmDTO);
+        _context.Films.Add(film);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetFilmById), new { id = film.Id }, film);
+    }
 
 
-        public FilmController(FilmContext context,IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-        }
+    [HttpGet]
+    public IEnumerable<Film> GetFilms([FromQuery] int skip =0, [FromQuery] int take=5)
+    {
+        return _context.Films.Skip(skip).Take(take);
+    }
 
-        [HttpPost]
-        public IActionResult AddFilm([FromBody] CreateFilmDTO filmDTO)
-        {
-            Film film = _mapper.Map<Film>(filmDTO);
-            _context.Films.Add(film);
-            _context.SaveChanges();
-            return CreatedAtAction(nameof(GetFilmById), new { id = film.Id }, film);
-        }
+    [HttpGet("{Id}")]
+    public IActionResult GetFilmById(int id)
+    {
+        var film =  _context.Films.FirstOrDefault(f => f.Id == id);
+        if (film == null) return NotFound();
+        return Ok(film);
 
-
-        [HttpGet]
-        public IEnumerable<Film> GetFilms([FromQuery] int skip =0, [FromQuery] int take=5)
-        {
-            return _context.Films.Skip(skip).Take(take);
-        }
-
-        [HttpGet("{Id}")]
-        public IActionResult GetFilmById(int id)
-        {
-            var film =  _context.Films.FirstOrDefault(f => f.Id == id);
-            if (film == null) return NotFound();
-            return Ok(film);
-
-        }
     }
 }
