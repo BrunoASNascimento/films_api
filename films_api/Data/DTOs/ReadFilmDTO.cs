a616eec893adca1ad063d3b1c63ae50d2cd1d0df
BrunoASNascimento <bruno-asn@outlook.com>
2025-03-01 23:04:02 -0300

    Add ReadFilmDTO for film data representation

---
+++ films_api/Data/DTOs/ReadFilmDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace films_api.Data.DTOs;
+
+public class ReadFilmDTO
+{
+
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public int Duration { get; set; }
+
+    public DateTime ReadTime { get; set; } = DateTime.Now;
+}
