92b86b3f7a14d7f71cefd16f301c4ce8720747e1
BrunoASNascimento <bruno-asn@outlook.com>
2025-03-01 23:21:59 -0300

    Enhance API configuration with Swagger integration and Newtonsoft.Json support

--- films_api/Program.cs
+++ films_api/Program.cs
@@ -1,14 +1,18 @@
+using System.Reflection;
 using films_api.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString =  builder.Configuration.GetConnectionString("FilmConnection");
+var connectionString = builder.Configuration.GetConnectionString("FilmConnection");
 
 builder.Services.AddDbContext<FilmContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 // Add services to the container.
 
@@ -20,11 +24,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API V1");
+    c.RoutePrefix = string.Empty; // Para abrir o Swagger na raiz do site
+});
 
 app.UseHttpsRedirection();
 
